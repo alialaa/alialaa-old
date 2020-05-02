638439d5c7e63494de0b197ca6d963f88cbe893d
Ali Alaa <hello@alialaa.com>
2020-05-02 23:03:15 +0200

    :wrench: Added layout-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
         author: `@gatsbyjs`
     },
     plugins: [
+        {
+            resolve: `gatsby-plugin-layout`,
+            options: {
+                component: require.resolve(`./src/components/layout.tsx`)
+            }
+        },
         `gatsby-plugin-typescript`,
         `gatsby-plugin-react-helmet`,
         {
