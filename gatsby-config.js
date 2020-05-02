7f3f57e840f1fb591256ee9cfdbfcbc8b26064c1
Ali Alaa <hello@alialaa.com>
2020-05-02 12:54:58 +0200

    :sparkles: Added offline and manifest plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,17 +19,23 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Ali Alaa`,
+                short_name: `alialaa`,
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+                icon: `src/images/icon.png`,
+                cache_busting_mode: `none`
+            }
+        },
+        {
+            resolve: `gatsby-plugin-offline`,
+            options: {
+                workboxConfig: {
+                    globPatterns: ["**/*"]
+                }
             }
         }
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
     ]
 };
